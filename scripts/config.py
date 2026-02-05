dd243762c8309e095f4258f6803f401e57fb4870
JORDAN XIMENES COSTA <64237882+dannjor@users.noreply.github.com>
2026-02-05 18:43:50 -0300

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -5,15 +5,16 @@ DATABASES = {
         'host': 'database-empresa-b.cs5w6cyu0cgy.us-east-1.rds.amazonaws.com',
         'port': 5432,
         'user': 'postgres',
-        'password': 'postgres123',
-        'database': 'postgres'
+        'password': '',
+        'database': ''
     },
     'sqlserver_local': {
         'server': 'DESKTOP-BSS415J\SQLEXPRESS01',
         'database': 'database_empresa_a',
         'driver': 'ODBC Driver 18 for SQL Server',
-        'UID': 'airflow_user',
-        'PWD': 'zse$5rdX01',
+        'UID': '',
+        'PWD': '',
         'Encrypt': 'no'
     }
+
 }
